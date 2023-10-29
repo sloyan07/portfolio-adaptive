46c55173cf16aa0f992829953d589db61d5df8b4
Kniaz__Sloyan <sloyan07@list.ru>
2023-10-29 03:04:47 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-console.log(2 + 5);
-console.log(2 + '5');
-console.log( 2 + 10);
+
+// || или (true должно выведится в консоль)
+// && и (false должно вывести в консоль)
+// \! не (Всё наоборот !false=true; !true=false)  
+// 1=true 0=false;
+
+
+let message = "Привет";
+
+let userAge = 5;
+
+
+if (1 === '1'){
+    console.log(message);
+}
+
+    
+
